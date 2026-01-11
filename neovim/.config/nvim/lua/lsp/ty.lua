96c4dd397e400cc2f8b90147a3819a7092cffea6
Josue Figueroa <poor.rican@pm.me>
2026-01-11 09:49:39 -0700

    feat(nvim): Add ty LSP config

---
+++ neovim/.config/nvim/lua/lsp/ty.lua
@@ -0,0 +1,17 @@
+return {
+	root_markers = { "uv.lock", "pyproject.toml", ".git" },
+	settings = {
+		ty = {
+			disableLanguageServices = true,
+			python = (function()
+				local cwd = vim.fn.getcwd()
+				if vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+					return cwd .. "/.venv/bin/python"
+				elseif vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+					return cwd .. "/venv/bin/python"
+				end
+				return vim.fn.exepath("python3") or "python3"
+			end)(),
+		},
+	},
+}
