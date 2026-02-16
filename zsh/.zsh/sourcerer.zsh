172df2e3b2f75336bb7be320d4a2494d25cb6019
Josue Figueroa <poor.rican@pm.me>
2026-02-16 11:50:19 -0700

    fix(zsh): Fix sourcerer theme

--- zsh/.zsh/sourcerer.zsh
+++ zsh/.zsh/sourcerer.zsh
@@ -18,13 +18,13 @@
 if [ "$TERM" = "linux" ]
 then
     echo -en "\e]P0222222" #black
-    echo -en "\e]P8111111" #darkgrey
+    echo -en "\e]P8272822" #darkgrey
     echo -en "\e]P1aa4450" #darkred
     echo -en "\e]P9ff6a6a" #red
     echo -en "\e]P2719611" #darkgreen
     echo -en "\e]PAb1d631" #green
-    echo -en "\e]P3cc8800" #brown
-    echo -en "\e]PBff9800" #yellow
+    echo -en "\e]P3ff9800" #brown
+    echo -en "\e]PB87875f" #yellow
     echo -en "\e]P46688aa" #darkblue
     echo -en "\e]PC90b0d1" #blue
     echo -en "\e]P58f6f8f" #darkmagenta
