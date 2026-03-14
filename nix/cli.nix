9257c67f6f66a7a92296b93c05dbc8b60e393306
Josue Figueroa <poor.rican@pm.me>
2026-03-13 22:51:11 -0700

    nix: add imsg overlay for darwin home-manager

--- nix/cli.nix
+++ nix/cli.nix
@@ -1,19 +1,23 @@
 # General-purpose CLI utilities
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-    bat
-    jq
-    fzf
-    tree
-    yazi
-    nmap
-    coreutils-full
-    rclone
-    mosh
-    stow
-    graphviz
-  ];
+  home.packages =
+    (with pkgs; [
+      ripgrep
+      fd
+      bat
+      jq
+      fzf
+      tree
+      yazi
+      nmap
+      coreutils-full
+      rclone
+      mosh
+      stow
+      graphviz
+    ])
+    ++ lib.optionals pkgs.stdenv.isDarwin [
+      pkgs.imsg
+    ];
 }
