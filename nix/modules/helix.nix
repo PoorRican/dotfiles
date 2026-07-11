871b406b354fa6ec94ac15493b07cdb4454c63a0
Josue Figueroa <poor.rican@pm.me>
2026-07-10 19:07:41 -0700

    feat(helix): configure PKM language servers

--- nix/modules/helix.nix
+++ nix/modules/helix.nix
@@ -3,4 +3,5 @@
 {
   home.packages = [ pkgs.helix ];
   xdg.configFile."helix/languages.toml".source = dotfiles + "/configs/helix/languages.toml";
+  xdg.configFile."helix/config.toml".source = dotfiles + "/configs/helix/config.toml";
 }
