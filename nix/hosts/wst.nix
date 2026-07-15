933a5deb2cb7b9ba41343113197fdf6ba2883208
Josue Figueroa <poor.rican@pm.me>
2026-07-15 02:16:54 -0700

    feat(nix.hosts): add `k3s` to `wst` config

--- nix/hosts/wst.nix
+++ nix/hosts/wst.nix
@@ -1,14 +1,18 @@
 # wst server
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ../profiles/minimal.nix
-    ../profiles/shell.nix
+../profiles/shell.nix
     ../profiles/server.nix
 		../profiles/dev-core.nix
 		#../profiles/dev-extra.nix
 		# not used at this moment
 		#../layers/knowledge-tools.nix
   ];
+
+	home.packages = with pkgs; [
+		k3s
+	]
 }
