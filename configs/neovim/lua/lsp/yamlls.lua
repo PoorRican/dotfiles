99d625ff0d870c8f344a0cf9206bdc41faa0804a
Josue Figueroa <poor.rican@pm.me>
2026-07-15 09:28:29 -0700

    feat(configs.nvim): add k8s YAML schema to language server

--- configs/neovim/lua/lsp/yamlls.lua
+++ configs/neovim/lua/lsp/yamlls.lua
@@ -1,3 +1,31 @@
-return function(capabilities)
-	capabilities.textDocument.completion.completionItem.snippetSupport = true
-end
+local schemas = require("schemastore").yaml.schemas()
+
+-- SchemaStore supplies kustomization.yaml; scope the generic Kubernetes schema
+-- to conventional manifest directories so unrelated YAML is not misdiagnosed.
+schemas.kubernetes = {
+	"**/k3s/**/*.yaml",
+	"**/k3s/**/*.yml",
+	"**/k8s/**/*.yaml",
+	"**/k8s/**/*.yml",
+	"**/kubernetes/**/*.yaml",
+	"**/kubernetes/**/*.yml",
+	"**/kustomize/**/*.yaml",
+	"**/kustomize/**/*.yml",
+	"**/manifests/**/*.yaml",
+	"**/manifests/**/*.yml",
+}
+
+return {
+	settings = {
+		yaml = {
+			completion = true,
+			hover = true,
+			schemaStore = {
+				enable = false,
+				url = "",
+			},
+			schemas = schemas,
+			validate = true,
+		},
+	},
+}
