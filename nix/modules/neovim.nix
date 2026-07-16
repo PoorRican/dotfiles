5084ef9dad64d7a562b7203b426d068f445a9369
Josue Figueroa <poor.rican@pm.me>
2026-07-15 18:21:24 -0700

    chore(nvim): cleanup LSPs

--- nix/modules/neovim.nix
+++ nix/modules/neovim.nix
@@ -26,15 +26,12 @@
       withPerl = true;
       withNodeJs = false;
     })
+    pkgs.tree-sitter
     pkgs.markdown-oxide
     pkgs.nixd
     pkgs.nixfmt
-    pkgs.tree-sitter
 		pkgs.clang-tools
-		pkgs.pylsp
 		pkgs.ruff
-		pkgs.python-lsp-ruff
-		pkgs.python-lsp-ruff
 		pkgs.pyrefly
   ];
 }
